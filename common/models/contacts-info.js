ffcdbac084b67228f40bf7932ff8d4cb26ff47cb
MebatsionSa <mebatsionsahle@gmail.com>
2020-08-19 11:43:43 +0300

    Eigth commit

--- common/models/contacts-info.js
+++ common/models/contacts-info.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Contactsinfo) {
-
+    // Operation hook to handle create and update info of date
+    Contactsinfo.observe('before save', function filterProperties(ctx, next) {
+        if(ctx.isNewInstance) {
+            ctx.instance.createdAt = new Date()
+        } else {
+            ctx.data.updatedAt = new Date()
+        } next();
+    });
 };
--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,3 +1,9 @@
+/**
+ *          MySQL Corner
+ * 
+ * // MySQL database sample model and autoupdate
+ * 
+ */
 module.exports = function(app) {
     app.dataSources.userDs.autoupdate('User', function(err) {
         if (err) throw err;
@@ -42,6 +48,63 @@ module.exports = function(app) {
         // }], function(err, StorageFiles) {
         //     if(err) throw err;
         //     console.log('StorageFile: \n', StorageFiles)
+        // });
+    });
+};
+
+/**
+ *          POSTGRESQL Corner
+ * 
+ * // Postgresql database sample model and autoupdate
+ * 
+ */
+
+module.exports = function(app) {
+    app.dataSources.cipgsds.autoupdate('Contacts_Info', function(err) {
+        if(err) throw err;
+
+        // app.models.Contacts_Info.create([{
+        //     BasicInfo: 'Mebatsion Sahle',
+        //     Email: 'mebgee@gmail.com',
+        //     PhoneNumber: '+251938000002',
+        //     Address: 'Alembank',
+        //     WorkingArea: 'Web Development',
+        //     picture: 'OO.png'
+        // }], function(err, Contacts_Infos) {
+        //     if(err) throw err;
+
+        //     console.log('ContactsInfo model: \n', Contacts_Infos);
+        // });
+    });
+};
+
+module.exports = function(app) {
+    app.dataSources.fspgsds.autoupdate('StorageFile', function(err) {
+        if (err) throw err;
+        
+        // app.models.StorageFile.create([{
+        //     name: 'OO.jpeg',
+        //     type: 'image/jpeg',
+        //     url: '/api/containers/common/download/OO.jpeg'
+        // }], function(err, StorageFiles) {
+        //     if(err) throw err;
+        //     console.log('StorageFile: \n', StorageFiles)
+        // });
+    });
+};
+
+module.exports = function(app) {
+    app.dataSources.usrpgsds.autoupdate('User', function(err) {
+        if (err) throw err;
+
+        // app.models.User.create([{
+        //     username: 'Meba',
+        //     email: 'mebgee@gmail.com',
+        //     password: 'l00pback'
+        // }], function(err, Users) {
+        //     if(err) throw err;
+
+        //     console.log('User model: \n', Users);
         // });
     });
 };
